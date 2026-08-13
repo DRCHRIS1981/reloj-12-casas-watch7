cf9d879f37dd94aa09afb03743e90a4c040b17c5
DRCHRIS1981 <christianperezdelap@gmail.com>
2026-08-13 17:41:48 -0600

    V7.5: actualizar versionCode y versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.drchris.reloj12casas"
         minSdk = 33
         targetSdk = 35
-        versionCode = 11
-        versionName = "7.4"
+        versionCode = 12
+        versionName = "7.5"
     }
 }
