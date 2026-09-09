7d2fbc1b8f5ba4a82de01b33f46b6eca0ee9077f
DRCHRIS1981 <christianperezdelap@gmail.com>
2026-09-09 10:40:52 -0600

    Configurar módulo Android

---
+++ traslado-app/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.traslados.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.traslados.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
